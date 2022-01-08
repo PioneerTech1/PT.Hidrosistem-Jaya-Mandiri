0e89336100f511b33e09becbcf2fc633543775fe
DidanPutra53 <didanputra75@gmail.com>
2022-01-09 06:29:25 +0700

    Fixed Bugs

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,6 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
     }).then(con => {
         console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
     })
